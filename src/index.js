f87828d54038c57f96ebc600290a4228fcbb3eb4
Whads1985 <648802888@qq.com>
2024-07-10 01:06:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ouxiang.me": dockerHub,
+  "quay.ouxiang.me": "https://quay.io",
+  "gcr.ouxiang.me": "https://gcr.io",
+  "k8s-gcr.ouxiang.me": "https://k8s.gcr.io",
+  "k8s.ouxiang.me": "https://registry.k8s.io",
+  "ghcr.ouxiang.me": "https://ghcr.io",
+  "cloudsmith.ouxiang.me": "https://docker.cloudsmith.io",
+  "ecr.ouxiang.me": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ouxiang.me": dockerHub,
 };
 
 function routeByHosts(host) {
